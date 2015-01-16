80905d2359f773705339e0b8a2df971c762c031d
Michael Kaminsky <mkaminsky11@gmail.com>
2015-01-16 13:58:36 -0500

    Update movie.js

--- import/movie.js
+++ import/movie.js
@@ -3,17 +3,36 @@ var request = require("request");
 
 exports.init = function(arr, user, text, callback){
 	try{
+		var out = text.replace(nick + " movie","").replace(nick + ", movie", "").trim();
+		out = out.toLowerCase().split(" ").join("+");
 		
-		
-		request('', function (error, response, body) {
+		request('http://www.omdbapi.com/?t=' + out, function (error, response, body) {
 			try{
-			  
+			  if (!error && response.statusCode == 200) {
+				var json = JSON.parse(body);
+				if(!!json.Title){
+					//title is there
+					var title = json.Title;
+					var year = json.Year;
+					var plot = json.Plot;
+					var rate = json.imdbRating;
+					
+					var to_print = title + " was released in " + year + ", and recieved a rating of " + rate + " on IMDB. Plot: " + plot;
+					callback(to_print, false);
+				}
+				else{
+					
+				}
+			  }
+			  else{
+				  callback("Movie not found", false);
+				}
 			} catch(e){
-				
+				callback("Movie not found", false);
 			}	
 		});
 	}
 	catch(e){
-		
+		callback("Movie not found", false);
 	}
 };
