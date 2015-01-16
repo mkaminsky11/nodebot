2acab859fafdfcfd76560c1e5765ee5691b5d6c5
Michael Kaminsky <mkaminsky11@gmail.com>
2015-01-16 13:49:32 -0500

    Create movie.js

---
+++ import/movie.js
@@ -0,0 +1,19 @@
+var nick = require("./nick.js").nick;
+var request = require("request");
+
+exports.init = function(arr, user, text, callback){
+	try{
+		
+		
+		request('', function (error, response, body) {
+			try{
+			  
+			} catch(e){
+				
+			}	
+		});
+	}
+	catch(e){
+		
+	}
+};
